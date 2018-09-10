7531fdfb3983d2cb917d321c5b134d9077134bf5
huanghaohao <huanghao89287503@163.com>
2018-09-10 17:02:05 +0800

    控制层 微调
    Signed-off-by: huanghaohao <huanghao89287503@163.com>

--- cdz-boot/cdz-admin/src/main/java/com/ga/cdz/controller/member/MemberListController.java
+++ cdz-boot/cdz-admin/src/main/java/com/ga/cdz/controller/member/MemberListController.java
@@ -13,7 +13,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.util.List;
+
 
 /**
  * @author huanghaohao
