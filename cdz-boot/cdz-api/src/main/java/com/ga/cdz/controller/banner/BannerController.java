f4a11db68817a6ebd37afc1b3a66724c1bcb4e24
liuyi <474766481@qq.com>
2018-09-11 15:16:51 +0800

    客户端Banner 1.1.0 修改request模式

--- cdz-boot/cdz-api/src/main/java/com/ga/cdz/controller/banner/BannerController.java
+++ cdz-boot/cdz-api/src/main/java/com/ga/cdz/controller/banner/BannerController.java
@@ -4,7 +4,7 @@ import com.ga.cdz.controller.AbstractBaseController;
 import com.ga.cdz.domain.bean.Result;
 import com.ga.cdz.domain.entity.Banner;
 import com.ga.cdz.service.IBannerService;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +24,7 @@ public class BannerController extends AbstractBaseController {
     @Resource
     private IBannerService iBannerService;
 
-    @GetMapping("/list")
+    @PostMapping("/list")
     public Result getListAllBanner() {
         List<Banner> banners = iBannerService.getListAllBanner();
         return Result.success().data(banners);
